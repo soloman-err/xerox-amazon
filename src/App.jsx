69d092289d9cbf25de9aa161e4b12126b8b6a026
shaaheensaab <shaaheensaab@gmail.email>
2023-03-27 02:18:57 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import Header from './component/Header/Header'
+
+function App() {
+  const [count, setCount] = useState([])
+
+  useEffect(() => {
+    fetch('products.json')
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, [])
+
+  return (
+    <div className="App">
+      {/* {
+        count.map(product=>)
+      } */}
+      <Header></Header>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../../images/Logo.svg';
+
+const Header = () => {
+    return (
+        <div className='nav
+         bg-slate-700 flex items-center justify-between px-32 py-3'>
+            <img className='h-full' src={logo} alt="" />
+            <div className='flex gap-10'>
+                <a className='hover:text-orange-600' href="/shop">Shop</a>
+                <a className='hover:text-orange-600' href="/orders">Orders</a>
+                <a className='hover:text-orange-600' href="/inventory">Inventory</a>
+                <a className='hover:text-orange-600' href="/login">Login</a>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
