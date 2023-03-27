ba685ebfae65040b6026463cabc4e8821b5aecb3
shaaheensaab <shaaheensaab@gmail.email>
2023-03-27 15:20:38 +0600

    products cards modified

--- src/component/Product/Product.jsx
+++ src/component/Product/Product.jsx
@@ -4,22 +4,22 @@ const Product = (props) => {
     const { img, name, price, seller, ratings } = props.product;
     return (
         <>
-            <div className="card bg-base-100 shadow-xl border-2 border-slate-200 p-2">
+            <div className="card bg-base-100 shadow-xl border-2 border-slate-300">
                 <figure><img className='' src={img} alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="text-lg lg:text-xl font-bold mb-6">{name}</h2>
-                    <div className='mt-auto space-y-2'>
-                        <div>
-                            <p className=''>Manufacturer: {seller}</p>
-                            <p className=''>Rating: {ratings} star</p>
+                <div className='mb-16'>
+                    <div className='px-2 py-2'>
+                        <div className='space-y-2'>
+                            <h2 className="text-lg lg:text-xl font-bold">{name}</h2>
+                            <h2 className='md:text-xl font-bold'>Price: ${price}</h2>
                         </div>
-                        <div className='flex justify-between items-center'>
-                            <h2 className='md:text-xl font-semibold'>Price: ${price}</h2>
-                            <div className="card-actions justify-end">
-                                <button className="p-2 text-sm xl:text-lg font-semibold rounded-md btn-warning">Add to cart</button>
+                        <div className=''>
+                            <div>
+                                <p className=''>Manufacturer: {seller}</p>
+                                <p className=''>Rating: {ratings} star</p>
                             </div>
                         </div>
                     </div>
+                    <button className='bg-warning hover:bg-orange-200 w-full py-3 rounded-b-lg absolute bottom-0 lg:text-xl font-bold'>Add to cart</button>
                 </div>
             </div>
         </>
