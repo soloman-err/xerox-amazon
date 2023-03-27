aaa45980ae3f88c6d55827f6a1adaed0db71056f
shaaheensaab <shaaheensaab@gmail.email>
2023-03-27 16:48:42 +0600

    checked & updated few state

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import Header from './component/Header/Header'
 import Shop from './component/Shop/Shop'
 
 function App() {
-  const [count, setCount] = useState([])
-
-  // useEffect(() => {
-  //   fetch('products.json')
-  //     .then(res => res.json())
-  //     .then(data => console.log(data))
-  // }, [])
-
   return (
     <div className="App">
       <Header></Header>
